350423c20a4ad42eef3a602d44697cd0a97e3f90
yyyyyylyyyyyy <34049024+yyyyyylyyyyyy@users.noreply.github.com>
2018-10-19 20:09:31 +0800

    Update urls.py

--- adminpage/urls.py
+++ adminpage/urls.py
@@ -20,26 +20,3 @@ urlpatterns = [
     url(r'^activity/menu?$', Activitymenu.as_view()),
     url(r'^activity/checkin?$', Activitycheckin.as_view()),
 ]
-
-# -*- coding: utf-8 -*-
-#
-from django.conf.urls import url
-
-from adminpage.views import *
-
-
-__author__ = "Epsirom"
-
-
-urlpatterns = [
-    url(r'^login?$', Login.as_view()),
-    url(r'^logout?$', Logout.as_view()),
-    url(r'^activity/list?$', ActivityList.as_view()),
-    url(r'^activity/delete?$', ActivityDelete.as_view()),
-    url(r'^activity/create?$', CreateActivity.as_view()),
-    url(r'^image/upload?$', ImageUpload.as_view()),
-    url(r'^activity/detail?$', ActivityDetail.as_view()),
-    url(r'^activity/menu?$', ActivityMenu.as_view()),
-    url(r'^activity/checkin?$', ActivityCheckin.as_view()),
-]
-
